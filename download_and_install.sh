d48d0f4eb2a550d5083e160fa2769ee395f3af94
Jordi Smit <jordismit20000@gmail.com>
2021-12-18 15:12:55 +0100

    unattached

--- download_and_install.sh
+++ download_and_install.sh
@@ -24,7 +24,7 @@ echo "# Setting up ohmyzsh #"
 echo "######################"
 echo ""
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
 chsh -s /bin/zsh
 
 sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
